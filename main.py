30ae4f34184de8a6d58fbfbba8df292fa19b164b
vastevenson <35041906+vastevenson@users.noreply.github.com>
2022-04-17 10:41:51 -0700

    Comments code for understanding

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import numpy as np
-from scipy.optimize import curve_fit
-import pylab as plt
+import numpy as np # np gives us the sine fn and math methods
+from scipy.optimize import curve_fit # method for optimizing params for best fit (recursion)
+import pylab as plt # for plotting data
 
 # Credit: https://stackoverflow.com/questions/16716302/how-do-i-fit-a-sine-curve-to-my-data-with-pylab-and-numpy, Vasco
 # y = A*sin(Bx+C)+D - figure out A,B,C,D such that we have best fitting sine fn to our data points
@@ -8,29 +8,37 @@ import pylab as plt
 # make some fake data with noise
 n = 1000 # number of data points
 x = np.linspace(0, 4*np.pi, n) # create a time vector, t (ind var)
-y = 3.0*np.sin(x+0.001) + 0.5 + np.random.randn(n) # create fake dep var with noise
+# true values
+A = 3
+B = 1
+C = 0.001
+D = 0.5
+y = A*np.sin(B*x+C) + D + np.random.randn(n) # create fake dep var with noise - thanks to randn
 
 # y = A*sin(Bx+C)+D
-guess_freq = 1 # freq is the B param
-guess_amplitude = 3*np.std(y)/(2**0.5) # amp is the A param
-guess_phase = 0 # phase is C param
-guess_offset = np.mean(y) # offset is D param
+guess_freq = 1 # freq is the B param - this is arbitrary
+guess_amplitude = np.std(y) # amp is the A param, note how our first guess will be
+guess_phase = 0 # phase is C param, arbitrary guess
+guess_offset = np.mean(y) # offset is D param (the vertical translation of the graph)
 
-p0=[guess_freq, guess_amplitude,
-    guess_phase, guess_offset]
 
-# create the function we want to fit
-def my_sin(x, freq, amplitude, phase, offset):
+p0=[guess_freq,
+    guess_amplitude,
+    guess_phase,
+    guess_offset]
+
+# define the custom function we want to fit
+def sin_fn(x, freq, amplitude, phase, offset):
     return np.sin(x * freq + phase) * amplitude + offset
 
 # now do the fit
-fit = curve_fit(my_sin, x, y, p0=p0)
+fit = curve_fit(sin_fn, x, y, p0=p0)
 
 # we'll use this to plot our first estimate. This might already be good enough for you
-y_first_guess = my_sin(x, *p0)
+y_first_guess = sin_fn(x, *p0)
 
 # recreate the fitted curve using the optimized parameters
-y_fit = my_sin(x, *fit[0])
+y_fit = sin_fn(x, *fit[0])
 
 plt.plot(y, '.')
 plt.plot(y_fit, label='after fitting')
